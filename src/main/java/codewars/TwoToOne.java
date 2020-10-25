958cc488a6915901f4c22c76fafd7b7ae6a858af
Eugene <eugen1390@gmail.com>
2020-10-25 22:12:35 +0100

    2 commit'

--- src/main/java/codewars/TwoToOne.java
+++ src/main/java/codewars/TwoToOne.java
@@ -1,12 +1,10 @@
 package codewars;
 
-import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class TwoToOne {
 
     public static String longest (String s1, String s2) {
-
         return makeOne(s1, s2)
                 .chars()
                 .distinct()
