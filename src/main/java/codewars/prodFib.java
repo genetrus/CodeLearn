db79e1c946a4157f2a82e04772eef8fdf7034c60
Eugene <eugen1390@gmail.com>
2020-10-27 15:41:06 +0100

    5 kyu. Product of consecutive Fib numbers

---
+++ src/main/java/codewars/prodFib.java
@@ -0,0 +1,39 @@
+package codewars;
+
+public class prodFib {
+
+    public static long[] productFib(long prod) {
+        int j = 0;
+        long[] coefficient = new long[3];
+
+        while (prod - prodTwoNear(j) > 0) {
+            j++;
+        }
+
+        coefficient[0] = fibo(j);
+        coefficient[1] = fibo(j + 1);
+        coefficient[2] = 0;
+
+        if (prod - prodTwoNear(j) == 0) {
+            coefficient[2] = 1;
+            return coefficient;
+        }
+        return coefficient;
+    }
+
+    private static long fibo(long i) {
+        long a = 0, b = 1, c;
+        if (i == 0)
+            return a;
+        for (int n = 2; n <= i; n++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+
+    private static long prodTwoNear(long j) {
+        return fibo(j) * fibo(j + 1);
+    }
+}
---
+++ src/test/java/codewars/prodFibTest.java
@@ -0,0 +1,18 @@
+package codewars;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class prodFibTest {
+    @Test
+    public void test1() {
+        long[] r = new long[] {55, 89, 1};
+        assertArrayEquals(r, prodFib.productFib(4895));
+    }
+
+    @Test
+    public void test2() {
+        long[] r = new long[] {89, 144, 0};
+        assertArrayEquals(r, prodFib.productFib(5895));
+    }
+}
