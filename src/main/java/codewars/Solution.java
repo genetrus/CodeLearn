ed509c3741f9e734b9f87ab4551c2cb991ec4b7a
Eugene <eugen1390@gmail.com>
2020-12-10 01:20:40 +0100

    update 1.11

---
+++ src/main/java/codewars/Solution.java
@@ -0,0 +1,10 @@
+package codewars;
+
+public class Solution {
+    public static String dashatize(int num) {
+        return String.valueOf(num).replaceAll("([13579])", "-$1-")
+                .replaceAll("--", "-")
+                .replaceAll("^-", "")
+                .replaceAll("-$", "");
+    }
+}
